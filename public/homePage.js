6b88fe2ce3f6f94b9f8f44909838512012bcf8f2
HelgaRoosh <vulfina@mail.ru>
2022-05-17 00:24:56 +0500

    кроме работа с избранным

--- public/homePage.js
+++ public/homePage.js
@@ -1,10 +1,90 @@
 "use strict"
+//npm start для запуска сервера
+//const { response } = require("express");
 
-const exit = new LogoutButton();
+/*
+ответ от сервера - пользователь:
+Object
+data: {created_at: '2019-10-15T05:28:25.593Z', login: 'oleg@demo.ru', password: 'demo', id: 1, balance: {…}}
+success: true
+[[Prototype]]: Object 
+*/
+
+const exit = new LogoutButton();//выход
 exit.action = () => {
-   ApiConnector.logout((callback) => {
-      if (callback.success) {
+   ApiConnector.logout((response) => {
+      if (response.success) {
          location.reload();
       }
+      
    });
 };
+
+new ProfileWidget();
+//почему const profileWidget = new ProfileWidget(); не работает с profileWidget.showProfile(response.data);?
+//У профиля статический метод. Объект создавать не нужно
+ApiConnector.current((response) => {//получение инф о пользователе
+   if (response.success) {
+      ProfileWidget.showProfile(response.data);
+   }
+   console.log(response);
+});
+
+function funcRatesBoard() { 
+   const tableBody = new RatesBoard();
+   ApiConnector.getStocks((response) => {
+      if (response.success) {
+         tableBody.clearTable();
+         tableBody.fillTable(response.data);
+      }
+      console.log(response);
+   }); 
+}
+funcRatesBoard();
+setInterval(funcRatesBoard, 60000);
+
+/*
+ответ от сервера - курсы валют:
+Object
+   data: {RUB_USD: 65.7916, RUB_EUR: 68.7835, RUB_NTC: 8.8095, USD_RUB: 0.0152, USD_EUR: 0.9565, …}
+   success: true
+[[Prototype]]: Object 
+*/
+
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = (data) => {
+   ApiConnector.addMoney(data, (response) => {      
+      if (response.success) {
+         ProfileWidget.showProfile(response.data);
+         moneyManager.setMessage(true, 'Пополнение баланса успешно');
+      } else {
+         moneyManager.setMessage(false, response.error);
+      }
+      console.log(response);
+   });
+};
+
+moneyManager.conversionMoneyCallback = (data) => {
+   ApiConnector.convertMoney(data, (response) => {
+   if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, 'Конвертирование успешно');
+   } else {
+      moneyManager.setMessage(false, response.error);
+   }
+   console.log(response);
+   });
+};
+
+moneyManager.sendMoneyCallback = (data) => {
+   ApiConnector.transferMoney(data, (response) => {
+   if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, 'Перевод валюты успешен');
+   } else {
+      moneyManager.setMessage(false, response.error);
+   }
+   console.log(response);
+   });
+};
+
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,35 +1,59 @@
 "use strict"
 
-let userForm = new UserForm();
+const userForm = new UserForm();
 userForm.loginFormCallback = function(data) {// функция, которая будет выполняться при попытке авторизации
 
-    /*data (объект, который содержит логин и пароль, введённые в форму, и который будет передаваться внутри loginFormAction).
-Функция должна выполнять запрос на сервер для попытки авторизации пользователя 
-(авторизацию пользователя выполняйте с помощью ApiConnector.login).
-Передайте в запрос авторизации функцию, которая будет выполняться при попытке авторизации. */
-    let callback = (response) => {//почему работает только стрелочной?
+    const callback = (response) => {//почему работает только стрелочной?
         console.log(response);
-        //Посмотрите в консоли, какой объект возвращает сервер. {success: true, userId: "1"}/false, data:'не найден'
-        if(!response.success) {//Проверьте успешность запроса.
-            this.setLoginErrorMessage(response.error);//??выводит сообщение с ошибкой при авторизации.В случае провала запроса выведите ошибку в окно для ошибок.
+    
+        if(!response.success) {
+            userForm.setLoginErrorMessage(response.error);//выводит сообщение с ошибкой при авторизации.В случае провала запроса выведите ошибку в окно для ошибок.
         } else {
-            location.reload();//В случае успеха запроса обновите страницу (с помощью location.reload();).
+            location.reload();//В случае успеха запроса обновите страницу
         }
     }
 
 ApiConnector.login(data, callback);//ApiConnector.login({ login, password }, callback) — запрос на авторизацию пользователя
-}
+};
 
 userForm.registerFormCallback = function(data) {
-    let callback = (response) => {
+    const callback = (response) => {
         console.log(response);
 
         if(!response.success) {
-            this.setRegisterErrorMessage(response.error);
+            userForm.setRegisterErrorMessage(response.error);
         } else {
             location.reload();
         }
     }
 
 ApiConnector.register(data, callback);
-}
+};
+
+/*
+ответ от сервера:
+{success: true, data: {…}}
+    data: {created_at: '2022-05-11T20:47:44.098Z', login: 'vulfina@mail.ru', password: 'demo', id: 6, balance: {…}}
+    success: true
+[[Prototype]]: Object
+    constructor: ƒ Object()
+    hasOwnProperty: ƒ hasOwnProperty()
+    isPrototypeOf: ƒ isPrototypeOf()
+    propertyIsEnumerable: ƒ propertyIsEnumerable()
+    toLocaleString: ƒ toLocaleString()
+    toString: ƒ toString()
+    valueOf: ƒ valueOf()
+    __defineGetter__: ƒ __defineGetter__()
+    __defineSetter__: ƒ __defineSetter__()
+    __lookupGetter__: ƒ __lookupGetter__()
+    __lookupSetter__: ƒ __lookupSetter__()
+    __proto__: (…)
+    get __proto__: ƒ __proto__()
+    set __proto__: ƒ __proto__() 
+    
+ИЛИ 
+{success: false, error: 'Пользователь c логином vulfina@mail.ru и указанным паролем не найден'}
+    error: "Пользователь c логином vulfina@mail.ru и указанным паролем не найден"
+    success: false
+[[Prototype]]: Object
+*/
