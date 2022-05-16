1327df2514b817db1d3332cde77aa9fb96d56b12
HelgaRoosh <96474422+HelgaRoosh@users.noreply.github.com>
2022-05-17 01:28:59 +0500

    Update loginPage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,19 +1,19 @@
 "use strict"
 
 const userForm = new UserForm();
-userForm.loginFormCallback = function(data) {// функция, которая будет выполняться при попытке авторизации
+userForm.loginFormCallback = function(data) {
 
-    const callback = (response) => {//почему работает только стрелочной?
+    const callback = (response) => {
         console.log(response);
     
         if(!response.success) {
-            userForm.setLoginErrorMessage(response.error);//выводит сообщение с ошибкой при авторизации.В случае провала запроса выведите ошибку в окно для ошибок.
+            userForm.setLoginErrorMessage(response.error);
         } else {
-            location.reload();//В случае успеха запроса обновите страницу
+            location.reload();
         }
     }
 
-ApiConnector.login(data, callback);//ApiConnector.login({ login, password }, callback) — запрос на авторизацию пользователя
+ApiConnector.login(data, callback);
 };
 
 userForm.registerFormCallback = function(data) {
@@ -29,31 +29,3 @@ userForm.registerFormCallback = function(data) {
 
 ApiConnector.register(data, callback);
 };
-
-/*
-ответ от сервера:
-{success: true, data: {…}}
-    data: {created_at: '2022-05-11T20:47:44.098Z', login: 'vulfina@mail.ru', password: 'demo', id: 6, balance: {…}}
-    success: true
-[[Prototype]]: Object
-    constructor: ƒ Object()
-    hasOwnProperty: ƒ hasOwnProperty()
-    isPrototypeOf: ƒ isPrototypeOf()
-    propertyIsEnumerable: ƒ propertyIsEnumerable()
-    toLocaleString: ƒ toLocaleString()
-    toString: ƒ toString()
-    valueOf: ƒ valueOf()
-    __defineGetter__: ƒ __defineGetter__()
-    __defineSetter__: ƒ __defineSetter__()
-    __lookupGetter__: ƒ __lookupGetter__()
-    __lookupSetter__: ƒ __lookupSetter__()
-    __proto__: (…)
-    get __proto__: ƒ __proto__()
-    set __proto__: ƒ __proto__() 
-    
-ИЛИ 
-{success: false, error: 'Пользователь c логином vulfina@mail.ru и указанным паролем не найден'}
-    error: "Пользователь c логином vulfina@mail.ru и указанным паролем не найден"
-    success: false
-[[Prototype]]: Object
-*/
